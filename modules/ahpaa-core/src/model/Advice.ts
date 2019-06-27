31a036ec8399217fe044ae51b9830bb1095ffeff
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-06-27 19:42:24 +0200

    Fix typo

--- modules/ahpaa-core/src/model/Advice.ts
+++ modules/ahpaa-core/src/model/Advice.ts
@@ -2,7 +2,7 @@ import ow from "ow";
 
 export interface Advice {
     patientName: string;
-    medicalproffesionalName: string;
+    medicalprofessionalName: string;
     parentPhoneNumber: string;
     uid?: string;
     advice: string;
@@ -14,8 +14,8 @@ export namespace Advice {
 
     export function validate(o: Advice) {
         ow(o, "Advice", ow.object);
-        ow(o.medicalproffesionalName, "Advice.patientName", ow.string.nonEmpty);
-        ow(o.patientName, "Advice.medicalproffesionalName", ow.string.nonEmpty);
+        ow(o.medicalprofessionalName, "Advice.medicalprofessionalName", ow.string.nonEmpty);
+        ow(o.patientName, "Advice.patientName", ow.string.nonEmpty);
         ow(o.parentPhoneNumber, "Advice.parentPhoneNumber", ow.string.numeric.length(9));
         ow(o.uid, "Advice.uid", ow.any(ow.undefined, ow.string.nonEmpty));
         ow(o.advice, "Advice.advice", ow.string.nonEmpty);
