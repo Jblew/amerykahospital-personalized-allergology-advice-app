66a914f77f90b7bc4a76fc75e9d379121f40b4ba
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-17 11:44:33 +0200

    feat: implement rate limiting in functions

--- modules/firebase-functions/src/index.ts
+++ modules/firebase-functions/src/index.ts
@@ -1,11 +1,49 @@
-import * as functions from "firebase-functions";
+// tslint:disable no-console
+
+import { Advice } from "amerykahospital-personalizedadvices-core";
 import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+import { FirebaseFunctionsRateLimiter } from "firebase-functions-rate-limiter";
 
-admin.initializeApp();
+import { AuthHelper } from "./helpers/AuthHelper";
+import { AddAdviceFunction } from "./logic/AddAdviceFunction";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+exports.addAdvice = functions.region("europe-west2").https.onCall(async (data, context) => {
+    const isMedicalProfessional =
+        context.auth &&
+        context.auth.uid &&
+        (await AuthHelper.isAuthenticatedMedicalProfessional(context.auth.uid, db as any));
+
+    if (!isMedicalProfessional) {
+        throw new functions.https.HttpsError(
+            "permission-denied",
+            "Only medicalprofessional accounts can access this function",
+        );
+    }
+
+    try {
+        await AddAdviceFunction.addAdvice(data as Advice, db as any);
+    } catch (e) {
+        console.error(e);
+        throw new functions.https.HttpsError("internal", "Error while adding advice: " + e);
+    }
+});
+
+exports.testRateLimiter = functions.https.onRequest(async (req, res) => {
+    console.log("Limiter.isQuotaExceededOrRecordCall");
+    const limiter = new FirebaseFunctionsRateLimiter(
+        { firebaseCollectionKey: "rate_limiter_collection", maxCallsPerPeriod: 2, periodSeconds: 15, debug: true },
+        db,
+    );
+    console.log("Limiter should have debug enabled");
+    const quotaExceeded = await limiter.isQuotaExceededOrRecordCall();
+    if (quotaExceeded) {
+        res.send("Sorry, quota exceeded");
+        return;
+    }
+
+    res.send("Function called");
+});
